2c46636c42a0b6bdeb54cfeeeeaa8c9d7921b2cc
BinkieBoy <123548589+BinkieBoy@users.noreply.github.com>
2023-01-25 13:07:23 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,8 +21,37 @@ self.addEventListener('install', e => {
 
 });    
 
-self.addEventListener('fetch', e => {
-      console.log('Service Worker: Fetching');
-        e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-      
+self.addEventListener('fetch', (ev) => {
+	ev.respondWith(
+	
+		// Deze functie wilt een Promise; 
+		// 'async function(){..}() wordt door JavaScript 
+		// AUTOMATISCH in een Promise verpakt! :-)
+		
+		async function() {
+	
+			console.log(`Request onderschept met data: `);
+			console.dir(ev.request);
+			
+			if (ev.request.url.indexOf('bgg')>-1){
+			
+				let oudRequest = ev.request;
+				let nieuweURL = oudRequest.url.replace('edwalter','stinow');
+			
+				let nieuwRequest = new Request(
+					nieuweURL,
+					{
+						method: oudRequest.method,
+						headers: oudRequest.headers,
+						mode: oudRequest.mode,
+						credentials: oudRequest.credentials
+					}
+				);
+			
+				return fetch(nieuwRequest); //Hier wordt een Promise verwacht
+			} else {
+				return fetch(ev.request);
+			}
+		}()
+	);
 });
